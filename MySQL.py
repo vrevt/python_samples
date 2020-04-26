7504672b443dcc4522d10ecbcfd9937521a18773
Иван Ревт <40318295+vrevt@users.noreply.github.com>
2020-04-26 12:37:47 +0300

    Update MySQL.py

--- MySQL.py
+++ MySQL.py
@@ -1,1 +1,26 @@
-TODO
+# install 
+# sudo apt update
+#sudo apt install mysql-server
+# sudo mysql_secure_installation
+
+# setting
+# sudo mysql_secure_installation
+
+# authentication (add password)
+# sudo mysql
+# mysql> SELECT user,authentication_string,plugin,host FROM mysql.user;
+# mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'new password';
+
+# applying changes:
+# mysql> FLUSH PRIVILEGES; 
+
+# mysql> exit
+
+# db entry
+# mysql -u root -p
+
+# show databases; 
+
+# create database test
+
+
