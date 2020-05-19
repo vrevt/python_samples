21d30ca61c20f609209c5c1120967744b1ba0303
Иван Ревт <40318295+vrevt@users.noreply.github.com>
2020-05-19 15:38:54 +0300

    Create rout.py

---
+++ rout.py
@@ -0,0 +1,9 @@
+@app.route("/delete", methods=['POST'])
+def delete():
+    cmd = "docker container prune -f"
+
+    try:
+        os.system(cmd)
+        return jsonify({"Status": "Deleted"})
+    except:
+        return jsonify({"Status": "Not Deleted"})
