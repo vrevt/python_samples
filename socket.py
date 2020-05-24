c64e0aa29b9ab0575f67f822ea585c252730e842
Иван Ревт <40318295+vrevt@users.noreply.github.com>
2020-05-24 16:30:04 +0300

    Create socket.py

---
+++ socket.py
@@ -0,0 +1,23 @@
+# for test start python file and use nc localhost 5000 to connect
+
+import socket 
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('localhost', 5000))
+server_socket.listen()
+
+while True:
+	print('before accept')
+	client_socket, addr = server_socket.accept()
+	print('connection from ', addr)
+
+	while True:
+		print('before recv')
+		request = client_socket.recv(4096)
+
+		if not request:
+			break 
+		else:
+			response = 'Hello world\n'.encode()
+			client_socket.send(response)
