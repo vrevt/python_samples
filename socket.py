dd8e58f2dedbc101fa84043ce87e0064da4c3f24
Иван Ревт <40318295+vrevt@users.noreply.github.com>
2020-05-24 17:52:39 +0300

    Update socket.py

--- socket.py
+++ socket.py
@@ -1,29 +1,43 @@
 import socket 
+from select import select
+
+to_monitor = []
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(('localhost', 5000))
 server_socket.listen()
 
+
 def accept_connection(server_socket):
-	while True:
-		client_socket, addr = server_socket.accept()
-		print('connection from ', addr)
-		send_message(client_socket)
+	client_socket, addr = server_socket.accept()
+	print('connection from ', addr)
+	to_monitor.append(client_socket)
 
 
 def send_message(client_socket):
+	request = client_socket.recv(4096)
+
+	if request:
+		response = 'Hello world\n'.encode()
+		client_socket.send(response)
+	else:
+		client_socket.close()
+
+
+def event_loop():
 	while True:
-		request = client_socket.recv(4096)
+		ready_to_read, _, _ = select(to_monitor, [], [])
 
-		if not request:
-			break 
-		else:
-			response = 'Hello world\n'.encode()
-			client_socket.send(response)
+		for sock in ready_to_read:
+			if sock is server_socket:
+				accept_connection(sock)
+			else:
+				send_message(sock)
 
-	client_socket.close()
 
 
 if __name__ == '__main__':
-  	accept_connection(server_socket)
+	to_monitor.append(server_socket)
+	event_loop()
+  	#accept_connection(server_socket)
