127400a2b8c3d77e6a269d730fafc03a8b27cc63
Ivan Revt <40318295+vrevt@users.noreply.github.com>
2021-01-16 19:39:19 +0300

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,21 @@
+# a - simple generator ( expression)
+
+a = (i for i in range(2, 8))
+print(a)
+
+for i in a:
+	print(i)
+
+# long entry with yield
+
+def gen(start, finish):
+	while (start < finish):
+		yield start * 0.33
+		start += 1
+
+a = gen(1, 4)
+print(a)
+for i in a:
+	print(i)
+
+
