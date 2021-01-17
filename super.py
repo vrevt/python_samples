e4c0c85d91f3fd016744788e142fa1b9dc9feff3
Ivan Revt <40318295+vrevt@users.noreply.github.com>
2021-01-17 18:03:01 +0300

    Update super.py

--- super.py
+++ super.py
@@ -22,5 +22,31 @@ class Cube(Square):
 		return face_area * self.length
 
 
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 *self.base* self.height
+
+
+class RightPyramid(Triangle, Square):
+    def __init__(self, base, slant_height):
+        self.base = base
+        self.slant_height = slant_height
+
+    def area(self):
+        base_area = super().area()
+        perimeter = super().perimeter()
+        return 0.5 *perimeter* self.slant_height + base_area
+
+
+
 cube = Cube(3)
 print(cube.surface_area())
+pyramid = RightPyramid(2, 4)
+
+# using mro - method resolution order.
+print(RightPyramid.__mro__)
+
