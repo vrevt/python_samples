ab32e5747a1d14c0fcbc4b7352b3945215b48712
vrevt <revtivan@gmail.com>
2023-05-01 19:52:30 +0300

    updated yield

--- yield.py
+++ yield.py
@@ -1,11 +1,25 @@
-# if you don`t need save all list of elements
+l = [1, 2, 3, 4, 5]
 
+# if you don`t need save all list of elements
 def gen(n):
     for i in range(n):
         yield i
 
+generator = gen(5)
+for i in generator:
+    print(i)
 
-a = gen(5)
-print(a)
-print(next(a))
-print(next(a))
+
+i = iter(l)
+print(next(i))
+print(next(i))
+
+
+# another way to create generator
+
+x = (i * i for i in l) # lazy calculation
+print(x)
+
+for i in x:
+    print(i)
+
