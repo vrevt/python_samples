06190f700876dcfb55ac12de16d623eb6b05f1dc
vrevt <revtivan@gmail.com>
2023-05-08 12:51:16 +0300

    added usePickle and updated zip_sample

--- pickle&shelve/usePickle.py
+++ pickle&shelve/usePickle.py
@@ -1,1 +1,20 @@
-# ToDo chapter 30
+import pickle
+
+class someClass():
+    attr = 1
+    def counter(self):
+        self.attr += 1
+
+
+filename = 'tmp.txt'
+
+object = someClass()
+file = open(filename, 'wb') # Создать внешний файл
+pickle.dump(object, file) # Сохранить объект в файле
+
+file = open(filename, 'rb')
+object = pickle.load(file) # Позднее извлечь обратно
+
+print(type(object))
+object.counter()
+print(object.attr)
